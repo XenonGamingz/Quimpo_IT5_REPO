5c9d1ff0f0903f788f79713f7c64e0510895501b
unknown <you@example.com>
2025-03-10 15:32:35 +0800

    Made changes to Battleship_v1.

--- battleship/battleship_v1.js
+++ battleship/battleship_v1.js
@@ -15,16 +15,17 @@ var isSunk = false;
 while(isSunk == false){
     guess = prompt("🚨Enemy ships approaching, set coordinates and fire!🚨 (Guess from 0-6)");
 
-    if (guess < 0 || guess > 6){
+    if (guess < 1 || guess > 6){
         alert("🚨Coordinates not  found, set the correct coordinates!🚨");
     }else {
         guesses = guesses + 1;
-        
+    
         if (guess == location1 || guess == location2 || guess == location3) {
+            alert('HIT!')
             hits = hits + 1;
 
             if (hits == 3) {
-                isSunk == true;
+                isSunk = true;
                 alert("💥Enemy ship has been sunk!💥");
             }
         }else {
