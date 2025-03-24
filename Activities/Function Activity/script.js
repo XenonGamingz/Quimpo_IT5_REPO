b62ca5ef9b82e854c39000d878a1ed501a6de920
XenonGamingz <lukequimpo@gmail.com>
2025-03-24 15:03:12 +0800

    Function Activity 1

---
+++ Activities/Function Activity/script.js
@@ -0,0 +1,13 @@
+function whatShallIWear(temperature){
+    if (temperature < 60) {
+        console.log("Wear a Jacket.")
+    } else if (temperature < 70){
+        console.log("Wear a Sweater.")
+    } else {
+        console.log("Wear a T-Shirt.")
+    }
+}
+
+whatShallIWear(50)
+whatShallIWear(80)
+whatShallIWear(60)
